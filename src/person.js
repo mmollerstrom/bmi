2f630f037098b2bf41f118347bd1c32b7408bac4
mmollerstrom <mmollerstrom@icloud.com>
2017-04-14 18:38:57 +0200

    add bmi calculation for person

--- src/person.js
+++ src/person.js
@@ -3,10 +3,12 @@ function Person(attr) {
   this.height = attr.height;
 };
 
-// Person.prototype.calculate_metric_bmi = function() {
-//   calculator = new BMICalculator();
-//   calculator.metric_bmi(this);
-// };
+Person.prototype.calculate_bmi = function() {
+  calculator = new BMICalculator();
+  calculator.metric_bmi(this);
+};
+
+
 // Person.prototype.calculate_standard_bmi = function() {
 //   calculator = new BMICalculator();
 //   calculator.standard_bmi(this);
