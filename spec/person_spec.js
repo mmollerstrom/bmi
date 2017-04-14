156d6ee9e859052a58f58396718a93f98670337a
mmollerstrom <mmollerstrom@icloud.com>
2017-04-14 17:01:53 +0200

    restart challenge to follow agile process

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -1,25 +1,25 @@
-describe("Person", function() {
-  var person;
-
-  beforeEach(function() {
-    person = new Person({weight: 90, height: 186});
-  });
-
-  it("Should calculate BMI value", function() {
-    person.calculate_bmi();
-    expect(person.bmiValue).toEqual(26.01);
-  });
-
-  it("should have BMI Message", function() {
-    person.calculate_bmi();
-    expect(person.bmiMessage).toEqual("Overweight");
-  });
-
-  it("should have weight of 90", function() {
-    expect(person.weight).toEqual(90);
-  });
-
-  it("should have height of 186", function() {
-    expect(person.height).toEqual(186);
-  });
-});
+// describe("Person", function() {
+//   var person;
+//
+//   beforeEach(function() {
+//     person = new Person({weight: 90, height: 186});
+//   });
+//
+//   it("Should calculate BMI value", function() {
+//     person.calculate_bmi();
+//     expect(person.bmiValue).toEqual(26.01);
+//   });
+//
+//   it("should have BMI Message", function() {
+//     person.calculate_bmi();
+//     expect(person.bmiMessage).toEqual("Overweight");
+//   });
+//
+//   it("should have weight of 90", function() {
+//     expect(person.weight).toEqual(90);
+//   });
+//
+//   it("should have height of 186", function() {
+//     expect(person.height).toEqual(186);
+//   });
+// });
