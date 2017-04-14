ca0a0b6b8ac5d98d9484a906e819404a5288b5df
mmollerstrom <mmollerstrom@icloud.com>
2017-04-14 16:51:15 +0200

    initiate src files

---
+++ src/person.js
@@ -0,0 +1,13 @@
+function Person(attr) {
+  this.weight = attr.weight;
+  this.height = attr.height;
+};
+
+Person.prototype.calculate_metric_bmi = function() {
+  calculator = new BMICalculator();
+  calculator.metric_bmi(this);
+};
+Person.prototype.calculate_standard_bmi = function() {
+  calculator = new BMICalculator();
+  calculator.standard_bmi(this);
+};
