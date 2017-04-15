b38491bd44f9e41d4e3d11ad3a6998fd67e918dc
mmollerstrom <mmollerstrom@icloud.com>
2017-04-15 13:26:36 +0200

    add UI, add spec in jasmine, test succes

---
+++ spec/bmi_ui_spec.js
@@ -0,0 +1,8 @@
+describe('BMI_UI - Index.html', function() {
+  beforeEach(function() {
+    jasmine.getFixtures().fixturesPath = '.';
+    loadFixtures('index.html');
+    $.holdReady(false);
+  });
+  
+});
