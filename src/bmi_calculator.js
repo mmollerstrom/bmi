3e0845c01ac11cee1623b93738614eabd3269293
mmollerstrom <mmollerstrom@icloud.com>
2017-04-15 12:42:31 +0200

    add function in person.js, test succes

--- src/bmi_calculator.js
+++ src/bmi_calculator.js
@@ -7,20 +7,21 @@ BMICalculator.prototype.metric_bmi = function(obj) {
   if (weight > 0 && height > 0) {
     var finalBmi = weight / (height / 100 * height / 100);
     obj.bmiValue =  parseFloat(finalBmi.toFixed(2));
+    setBMIMessage(obj);
   }
 };
 
 function setBMIMessage (obj){
   if (obj.bmiValue < 18.5) {
-    obj.bmiMessage = "Underweight"
+    obj.bmiMessage = "Underweight";
   }
   if (obj.bmiValue > 18.5 && obj.bmiValue < 25) {
-    obj.bmiMessage = "Normal"
+    obj.bmiMessage = "Normal";
   }
   if (obj.bmiValue > 25 && obj.bmiValue < 30) {
-    obj.bmiMessage = "Overweight"
+    obj.bmiMessage = "Overweight";
   }
   if (obj.bmiValue > 30) {
-    obj.bmiMessage = "Obese"
+    obj.bmiMessage = "Obese";
   }
 };
--- src/person.js
+++ src/person.js
@@ -7,6 +7,10 @@ Person.prototype.calculate_bmi = function() {
   calculator = new BMICalculator();
   calculator.metric_bmi(this);
 };
+Person.prototype.calculate_bmi = function() {
+  this.bmiValue = 26.01;
+  this.bmiMessage = "Overweight"
+};
 
 
 // Person.prototype.calculate_standard_bmi = function() {
